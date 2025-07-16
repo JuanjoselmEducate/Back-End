715f50326eed912539986e11addd3645295b73c7
Juanjo-LM-JRXS <juanlastre54@gmail.com>
2025-07-16 09:04:36 -0500

    Estructura inicial API

---
+++ src/main/java/co/edu/ue/api/OdsBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.ue.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/ue/api/OdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ue.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
