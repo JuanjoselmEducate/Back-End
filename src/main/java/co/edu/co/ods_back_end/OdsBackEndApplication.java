d6ca5370f3c762ae804bfe6cc0e2742cc4af7274
Juanjo-LM-JRXS <juanlastre54@gmail.com>
2025-09-01 18:41:06 -0500

    New Version API with new dependencys

---
+++ src/main/java/co/edu/co/ods_back_end/OdsBackEndApplication.java
@@ -0,0 +1,13 @@
+package co.edu.co.ods_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsBackEndApplication.class, args);
+	}
+
+}
