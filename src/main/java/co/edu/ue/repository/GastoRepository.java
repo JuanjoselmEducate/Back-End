b8bce8ebe8b5e6d456251ce57a40e9e43e58ce69
nicolas <npuentesd@uniempresarial.edu.co>
2025-09-08 19:56:08 -0500

    feat(gastos): entidad, repositorios, service y controller

--- src/main/java/co/edu/ue/repository/GastoRepository.java
+++ src/main/java/co/edu/ue/repository/GastoRepository.java
@@ -1,13 +1,5 @@
 package co.edu.ue.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import co.edu.ue.entity.Gasto;
-import java.util.Date;
-import java.util.List;
+public class GastoRepository {
 
-public interface GastoRepository extends JpaRepository<Gasto, Integer> {
-    List<Gasto> findByUsuarioId(int userId);
-    List<Gasto> findByCategoriaId(int categoriaId);
-    List<Gasto> findByTipoGastoIgnoreCase(String tipoGasto);
-    List<Gasto> findByFechaGastoBetween(Date inicio, Date fin);
 }
